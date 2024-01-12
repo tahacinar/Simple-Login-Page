e9e63d3c9b2f088f9b9bcd44e8d4faf3c2fae1fd
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-01-12 23:09:39 +0300

    v1.0.

--- app/src/main/java/com/tahacinar/loginscreen/MainActivity.kt
+++ app/src/main/java/com/tahacinar/loginscreen/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.tahacinar.loginscreen
 
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.tahacinar.loginscreen.databinding.ActivityMainBinding
 import android.os.Bundle as Bundle1
@@ -9,10 +11,10 @@ import android.os.Bundle as Bundle1
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     lateinit var username : EditText
     lateinit var password: EditText
     lateinit var loginButton: Button
-
 
     override fun onCreate(savedInstanceState: Bundle1?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +22,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
-
-
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
-
 }
